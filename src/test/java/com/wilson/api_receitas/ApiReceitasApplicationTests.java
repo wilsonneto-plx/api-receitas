728c5a1ccf746d418569979bde26f2708b6a8e80
Wilson de Andrade Veloso Neto <wilsonneto@aluno.uespi.br>
2026-07-28 23:02:56 -0300

    chore: initialize Spring Boot project

---
+++ src/test/java/com/wilson/api_receitas/ApiReceitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilson.api_receitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReceitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
