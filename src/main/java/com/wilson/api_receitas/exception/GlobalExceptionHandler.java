1a4ddbbfa77392206fa006a0bd493f978381c814
Wilson de Andrade Veloso Neto <wilsonneto@aluno.uespi.br>
2026-08-02 22:05:34 -0300

    feat: handle validation exceptions

--- src/main/java/com/wilson/api_receitas/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wilson/api_receitas/exception/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package com.wilson.api_receitas.exception;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -40,5 +43,23 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex){
+
+        List<FieldError> erros = ex.getFieldErrors();
+
+        var listaDeErrosFormatados = erros.stream().map(DadosErroValidacao::new).toList();
+
+        return ResponseEntity.badRequest().body(listaDeErrosFormatados);
+
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+
+    }
 
 }
